6a69d655e991b5cd202d62c60f91c538958d9a81
Kenneth Basjan <155391671+Kenneth-Basjan-Weberu@users.noreply.github.com>
2024-12-30 15:25:52 +0200

    Added Images

--- weberu/src/App.jsx
+++ weberu/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
+import Menu from "./components/Menu/Menu";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Updates from "./pages/updates/Updates";
+import Solutions from "./pages/solutions/Solutions";
+import Contact from "./pages/contact/Contact";
+
+import { AnimatePresence } from "framer-motion";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isDarkMenu = location.pathname === "/updates";
+
+  const pageTitles = {
+    "/": "Balanced Pitch | CG MWT NOV 2024",
+    "/about": "About Us | Balanced Pitch | CG MWT NOV 2024",
+    "/solutions": "Solutions | Balanced Pitch | CG MWT NOV 2024",
+    "/updates": "Updates | Balanced Pitch | CG MWT NOV 2024",
+    "/contact": "Contact | Balanced Pitch | CG MWT NOV 2024",
+  };
+
+  useEffect(() => {
+    const currentTitle = pageTitles[location.pathname] || "Aiden Brooks";
+    document.title = currentTitle;
+
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 750);
+  }, [location.pathname]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} isDark={isDarkMenu} />
+      <AnimatePresence mode="wait" initial={false}>
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/updates" element={<Updates />} />
+          <Route path="/solutions" element={<Solutions />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
+    </div>
+  );
 }
 
-export default App
+export default App;
